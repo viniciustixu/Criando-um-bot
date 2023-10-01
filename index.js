79ba208ea442dd0d70b2b4227f2ca916ea32839c
Vinicius Souza <120525353+viniciustixu@users.noreply.github.com>
2023-10-01 01:13:58 -0300

    Robo simples funcionando

--- index.js
+++ index.js
@@ -2,14 +2,23 @@ const puppeteer = require('puppeteer');
 
 
 async function robo()  {
-  // Launch the browser and open a new blank page
   const browser = await puppeteer.launch( {headless: false});
   const page = await browser.newPage();
 
-  // Navigate the page to a URL
-  await page.goto('https://twitter.com/');
+  const moedaBase = 'dolar';
+  const moedaFinal = 'real';
+  const url = `https://www.google.com.br/search?q=${moedaBase}+para+${moedaFinal}&sca_esv=569794731&sxsrf=AM9HkKlAGjizu4zGM72sQR4nRi9v6SPG8Q%3A1696132524815&source=hp&ei=rO0YZfvTHqWb1sQPw7SC4AE&iflsig=AO6bgOgAAAAAZRj7vKlVmvh-zoUiQtrt4eh6axES3KLm&ved=0ahUKEwj72fWE-tOBAxWljZUCHUOaABwQ4dUDCAo&uact=5&oq=dolar+para+real&gs_lp=Egdnd3Mtd2l6Ig9kb2xhciBwYXJhIHJlYWwyCBAAGIAEGLEDMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAESJEMUABYjgtwAHgAkAEAmAGcAaABvwyqAQQwLjE0uAEDyAEA-AEBwgIHECMYigUYJ8ICBBAjGCfCAgsQABiABBixAxiDAcICCxAuGIAEGMcBGNEDwgIOEC4YgAQYsQMYxwEY0QPCAgoQABiKBRixAxhDwgIHEAAYigUYQ8ICExAuGIoFGLEDGIMBGMcBGNEDGEPCAgkQABiKBRgKGEPCAhEQLhiABBixAxiDARjHARjRA8ICCxAuGIAEGLEDGIMB&sclient=gws-wiz`
+  
+
+  await page.goto(url);
   await page.screenshot({path: 'example.png'});
-  console.log('ok');
+  
+  const resultado = await page.evaluate(() => {
+    return document.querySelector('.lWzCpb.a61j6').value
+  })
+
+
+  console.log(`O valor de ${moedaBase} em ${moedaFinal} é ${resultado}`);
 
   await browser.close();
 }
