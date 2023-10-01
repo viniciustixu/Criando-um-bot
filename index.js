c2e8ee27ee77ac13d3d56ab0a58764c06838aef7
Vinicius Souza <120525353+viniciustixu@users.noreply.github.com>
2023-10-01 01:36:36 -0300

    Versão otimizada com readline

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const puppeteer = require('puppeteer');
+const readlineSync = require('readline-sync');
 
 
 async function robo()  {
-  const browser = await puppeteer.launch( {headless: false});
+  const browser = await puppeteer.launch( {headless: 'new'});
   const page = await browser.newPage();
 
-  const moedaBase = 'dolar';
-  const moedaFinal = 'real';
+  const moedaBase = readlineSync.question('Informe uma moeda base: ') || 'dolar';
+  const moedaFinal = readlineSync.question('Informe uma moeda final: ') || 'real';
   const url = `https://www.google.com.br/search?q=${moedaBase}+para+${moedaFinal}&sca_esv=569794731&sxsrf=AM9HkKlAGjizu4zGM72sQR4nRi9v6SPG8Q%3A1696132524815&source=hp&ei=rO0YZfvTHqWb1sQPw7SC4AE&iflsig=AO6bgOgAAAAAZRj7vKlVmvh-zoUiQtrt4eh6axES3KLm&ved=0ahUKEwj72fWE-tOBAxWljZUCHUOaABwQ4dUDCAo&uact=5&oq=dolar+para+real&gs_lp=Egdnd3Mtd2l6Ig9kb2xhciBwYXJhIHJlYWwyCBAAGIAEGLEDMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAESJEMUABYjgtwAHgAkAEAmAGcAaABvwyqAQQwLjE0uAEDyAEA-AEBwgIHECMYigUYJ8ICBBAjGCfCAgsQABiABBixAxiDAcICCxAuGIAEGMcBGNEDwgIOEC4YgAQYsQMYxwEY0QPCAgoQABiKBRixAxhDwgIHEAAYigUYQ8ICExAuGIoFGLEDGIMBGMcBGNEDGEPCAgkQABiKBRgKGEPCAhEQLhiABBixAxiDARjHARjRA8ICCxAuGIAEGLEDGIMB&sclient=gws-wiz`
   
 
